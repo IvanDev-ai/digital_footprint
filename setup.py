4c9712b7a7396ee3d082e454f331d415073384aa
IvanDev <123570888+IvanDev-ai@users.noreply.github.com>
2025-02-02 18:38:41 +0100

    Upgrade version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="digital_footprint",
-    version="0.1.0",
+    version="0.1.1",
     description="Digital Footprint Scanner: Free tool to extract useful information from an email.",
     author="ivanMartin",
     author_email="imartin.desarrollo@gmail.com",
