25bd913b544bf073eea445db058d126c8be2c4f2
IvanDev <123570888+IvanDev-ai@users.noreply.github.com>
2025-02-03 01:12:50 +0100

    Finish repo

--- setup.py
+++ setup.py
@@ -1,21 +1,23 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="digital_footprint",
-    version="0.1.1",
+    name="digital-footprint",
+    version="1.6.9",
     description="Digital Footprint Scanner: Free tool to extract useful information from an email.",
-    author="ivanMartin",
+    author="IvanMartin",
     author_email="imartin.desarrollo@gmail.com",
     packages=find_packages(),
     install_requires=[
-        "requests",
-        "beautifulsoup4",
-        "python-whois",
-        "dnspython"
+    "requests",
+    "python-whois",
+    "dnspython",
+    "waybackpy",
+    "colorama",
+    "googlesearch-python"
     ],
     entry_points={
     "console_scripts": [
-        "digital-footprint = mypackage.scanner:main"
+        "digital-footprint = digital_footprint.scanner:main"
     ]
 },
     classifiers=[
